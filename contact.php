af8dd518bce2c413ee40bcf12fc265ae7ca5a369
Elamathi Elangovan <elamathi.0409@gmail.com>
2023-04-14 05:52:56 -0600

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['submit'])) {
-    $to = "youremail@example.com"; // Enter your email address here
+    $to = "elamathi.0409@gmail.com"; // Enter your email address here
     $subject = $_POST['subject'];
     $name = $_POST['name'];
     $email = $_POST['email'];
